b5b4355d4612da6d570c2122e83719f39ac2a0e8
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-05-08 01:03:09 -0700

    Added a labels helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def span_labels(*text)
+    res = ""
+    text.each do |t|
+      res += '<span class="label">' + t + '</span> '
+    end
+    return (res).html_safe
+  end
 end
